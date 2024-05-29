66c20a837903f8b7e625d52e93b2b2c3c4dda568
Amir Mohammad Fakhimi <fakhimi.amirmohamad@gmail.com>
2024-05-29 23:40:27 +0330

    Add help

--- regex_generator/const.py
+++ regex_generator/const.py
@@ -5,14 +5,16 @@ INDEX_TABLE = lambda x: chr(x)
 
 ESCAPE = '{([])^$.|*+?}\\' + '\u061f'  # Inside () or Outside
 INESCAPE = '^-'  # Inside []
+DIGIT = string.digits
 # DIGIT = string.digits + '٠١٢٣٤٥٦٧٨٩'
-DIGIT = string.digits + '\u0660\u0661\u0662\u0663\u0664\u0665\u0666\u0667\u0668\u0669'
+# DIGIT = string.digits + '\u0660\u0661\u0662\u0663\u0664\u0665\u0666\u0667\u0668\u0669'
 UPPER = string.ascii_uppercase
 LOWER = string.ascii_lowercase
+LETTERS = string.ascii_letters
 # LETTERS = string.ascii_letters + 'ابتثجحخدذرزژسشصضطظعغفقكلمنهويءآأؤإئ'
-LETTERS = string.ascii_letters + ('\u0627\u0628\u062a\u062b\u062c\u062d\u062e\u062f\u0630\u0631\u0632\u0698\u0633'
-                                  '\u0634\u0635\u0636\u0637\u0638\u0639\u063a\u0641\u0642\u0643\u0644\u0645\u0646'
-                                  '\u0647\u0648\u064a\u0621\u0622\u0623\u0624\u0625\u0626')
+# LETTERS = string.ascii_letters + ('\u0627\u0628\u062a\u062b\u062c\u062d\u062e\u062f\u0630\u0631\u0632\u0698\u0633'
+#                                   '\u0634\u0635\u0636\u0637\u0638\u0639\u063a\u0641\u0642\u0643\u0644\u0645\u0646'
+#                                   '\u0647\u0648\u064a\u0621\u0622\u0623\u0624\u0625\u0626')
 WORD = LETTERS + DIGIT + '_'
 LOWER_HEXDIGIT = DIGIT + LOWER[:6]
 UPPER_HEXDIGIT = DIGIT + UPPER[:6]
--- skills/regex_generator.py
+++ skills/regex_generator.py
@@ -9,17 +9,37 @@ from regex_generator.generator import generator
 
 # https://github.com/maojui/Regex-Generator/tree/master?tab=readme-ov-file
 class RegexGenerator(Skill):
-    @match_regex(r'^/شروع')
+    @match_regex(r'^/generate')
     async def regex_generator(self, message):
-        POPULATION = 100
-        GENERATION = 2
+        message_split = message.text.split('\n')
 
-        message_split = message.text.split('\n')[1:]
+        message_split_space = message_split[0].split(' ')
+        if 1 < len(message_split_space) < 3 or len(message_split_space) > 3:
+            await message.respond('لطفاً تعداد جمعیت و نسل را وارد کنید.')
+            return
+        elif len(message_split_space) == 3:
+            try:
+                POPULATION = int(message_split.split(' ')[1])
+                GENERATION = int(message_split.split(' ')[2])
+            except ValueError:
+                await message.respond('لطفاً تعداد جمعیت و نسل را به صورت عدد وارد کنید.')
+                return
+        else:
+            POPULATION = 100
+            GENERATION = 2
+
+        if not 30 >= GENERATION >= 1:
+            await message.respond('لطفاً تعداد نسل را بین ۱ تا ۳۰ وارد کنید.')
+            return
+        if not 100 >= POPULATION >= 10:
+            await message.respond('لطفاً تعداد جمعیت را بین ۱۰ تا ۱۰۰ وارد کنید.')
+            return
+
+        message_split = message_split[1:]
         if len(message_split) == 0:
             await message.respond('لطفاً ورودی را وارد کنید.')
             return
 
-        result = []
         result = generator(message_split, POPULATION, GENERATION)
-        for fit, regex in sorted(set(result), key=lambda x: -x[0])[:20]:
-            await message.respond(f'{fit}\t\t{regex}')
+        for fit, regex in sorted(set(result), key=lambda x: -x[0])[:3]:
+            await message.respond(f'score: {fit}\t\t{regex}')
